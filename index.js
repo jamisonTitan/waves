4a886813a12d1420ce3ecb020d29de32747de1fa
Titan Jamison <jamisontitan@gmail.com>
2020-02-10 21:04:05 -0800

    finfished

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const RADIUS = 100;
 let currDeg = 0,
   lastDeg = 0;
-let x = RADIUS + 110,
+let x = 110,
   y = 0;
-let waveSegments = [];
-let stepSize = 10;
-const waveSegment = (xLast, yLast, x, y) => {
+let xLast = x,
+  yLast = y;
+let points = [];
+let stepSize = 5;
+const Point = (x, y, hor = false, back = false) => {
   (this.x = x), (this.y = y), (this.xLast = xLast), (this.yLast = yLast);
   return {
-    xLast,
-    yLast,
-    x,
-    y,
     draw: () => {
       stroke(255, 255, 255);
+      x++;
       //console.log(this.xLast, this.yLast, this.x, this.y);
-      line(this.xLast, this.yLast, this.x, this.y);
+      point(x, y);
+      //line(this.xLast, this.yLast, this.x, this.y);
       //point(110 + RADIUS, y);
     }
   };
@@ -30,18 +30,36 @@ function draw() {
   background(0);
   lastDeg = currDeg;
   currDeg += stepSize;
-  stepSize++;
+  for (let i = 0; i < currDeg - lastDeg; i++) {
+    x = RADIUS * cos(radians(currDeg + i)) + 110;
+    y = RADIUS * sin(radians(currDeg + i)) + height / 2;
+    points.push(Point(x, y));
+    points.push(Point(x, y, false, true));
+    points.push(Point(x, y, true));
+    points.push(Point(x, y, true, true));
+  }
+  /*
   x = RADIUS * cos(radians(currDeg)) + 110;
   y = RADIUS * sin(radians(currDeg)) + height / 2;
   xLast = RADIUS * cos(radians(lastDeg)) + 110;
   yLast = RADIUS * sin(radians(lastDeg)) + height / 2;
   noFill();
   //console.log(x, y, xLast, yLast);
-  waveSegments.push(waveSegment(xLast, yLast, x, y));
-  waveSegments.forEach(ws => ws.draw());
-  stroke(255, 0, 0);
+  points.push(Point(x, y));
+  */
+  noFill();
+  points.forEach(p => {
+    p.draw();
+  });
   strokeWeight(5);
-  line(110, height / 2, x, y);
   stroke(100, 100, 100);
   ellipse(110, height / 2, 200);
+  strokeWeight(10);
+  stroke(255, 0, 0);
+  point(x, y);
 }
+
+$("input").on("change", () => {
+  stepSize = $("input").val();
+  console.log(stepSize, $("input").val());
+});
